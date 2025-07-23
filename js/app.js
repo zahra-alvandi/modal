ebc31d7739f53faff29ac53a6364d3c11d3de02c
Zahra Alvandi <zalvandi39@gmail.com>
2025-07-23 12:45:17 +0330

    update app.js

--- js/app.js
+++ js/app.js
@@ -2,10 +2,12 @@ const logginBtn = document.querySelector(".login-button");
 const modalScreen = document.querySelector(".modal-screen");
 const closeXBtn = document.querySelector(".close-x-btn");
 const close = document.querySelector(".close");
+const continueBtn = document.querySelector(".continue");
 
-function escape() {
-    modalScreen.classList.add("hidden");
-}
+
+// function escape() {
+//     modalScreen.classList.add("hidden");
+// }
 
 
 logginBtn.addEventListener("click", function () {
@@ -18,3 +20,34 @@ closeXBtn.addEventListener("click", function () {
 close.addEventListener("click", function () {
     modalScreen.classList.add("hidden")
 });
+continueBtn.addEventListener("click", function () {
+    modalScreen.classList.add("hidden")
+});
+
+document.body.addEventListener("keyup", function (event) {
+    if (event.key === "Escape") {
+        modalScreen.classList.add("hidden");
+    }
+})
+
+// function showModal() {
+//     modalScreen.classList.remove("hidden");
+// }
+
+// function hideModal() {
+//     modalScreen.classList.add("hidden");
+// }
+
+// logginBtn.addEventListener("click", showModal());
+
+// close.addEventListener("click", hideModal());
+
+// closeXBtn.addEventListener("click", hideModal());
+
+// continueBtn.addEventListener("click", hideModal());
+
+// document.body.addEventListener("keyup", function (event) {
+//     if (event.key === "Escape") {
+//         hideModal();
+//     }
+// })
